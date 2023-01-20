0993a4a04be25ed03334b3ce6314105c09be7ef4
what-the-hek <108735805+what-the-hek@users.noreply.github.com>
2023-01-20 19:15:16 +0800

    added users

--- crowdfunding/crowdfunding/urls.py
+++ crowdfunding/crowdfunding/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include('projects.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('users/', include('users.urls')),
+    path('', include('projects.urls')),
 ]
--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -6,6 +6,7 @@ class PledgeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pledge
         fields = ['id', 'amount', 'comment', 'anonymous', 'project', 'supporter',]
+        read_only_fields = ['id', 'supporter']
         # or this does the same
         # fields = '__all__'
         
@@ -17,7 +18,7 @@ class ProjectSerializer(serializers.Serializer):
     image = serializers.URLField()
     is_open = serializers.BooleanField()
     date_created = serializers.DateTimeField()
-    owner = serializers.CharField(max_length=200)
+    owner = serializers.ReadOnlyField(source='owner_id')
 
     def create(self, validated_data):
         return Project.objects.create(**validated_data)
